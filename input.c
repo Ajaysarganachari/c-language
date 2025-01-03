2cd33f51455e037578b1c2d5300b346adc9cc30b
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-01-03 10:15:06 +0530

    Create input.c

---
+++ input.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter the value:");
+    scanf("%d",&num);
+    printf("num=%d\n",num);
+    
+    char c,temp;
+    printf("enter the char :");
+    scanf("%c",&temp);
+    scanf("%c",&c);
+    printf("c=%c\n",c);
+return 0;
+}
