317766d518da6641326fc29b0c247687c6dd30cf
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-01-03 10:55:01 +0530

    Create c1_if.c

---
+++ Condition/c1_if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter the value :");
+    scanf("%d",&num);
+    if(num>20){
+    printf("num=%d is grater than  20.",num);
+    }
+    printf("exit");
+    return 0;
+}
