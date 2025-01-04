7587d83cf972613318ff72e982ed2c3c5f9b7e7a
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-01-04 11:20:15 +0530

    Create switch.c

---
+++ Condition/switch.c
@@ -0,0 +1,27 @@
+
+                case 10:{
+            printf("10ltr.\n");
+            printf("enter a hot\n");
+            printf("enter b cold\n");
+            printf("enter c normal\n");
+             printf("enter the option:\n");
+            char o,temp;
+            scanf("%c",&temp);
+            scanf("%c",&o);
+        switch(o){
+                case 'a':{
+                    printf("drop filtered water");
+                }
+                case 'b':{
+                    printf("drop unfiltered water");
+                }
+                case 'c':{
+                    printf("drop corporation water");
+                }
+                break;
+                    default:{
+                        printf("invalid input");
+
+    }
+    }
+                }
