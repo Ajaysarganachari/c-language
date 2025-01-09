f47be19f14b157859edb77bc196660154985610d
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-01-09 11:06:52 +0530

    Create for_loop.c

---
+++ loops/for_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=10;i++){
+    printf("numbers=%d\n",i);
+
+    }
+    //table 
+   
+    for(int a=1;a<=10;a++){
+    printf("%d\n",a*5);
+    }
+    return 0;
+}
