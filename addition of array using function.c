4c0e34ca3671680d6c86d72c88f0277a74168ef7
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-01-22 11:56:35 +0530

    Update addition of array using function.c

--- addition of array using function.c
+++ addition of array using function.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
-
-int Array(int arr[], int size) {
-    int total = 0;
-    for (int i = 0; i < size; i++) {
-        total += arr[i];
+int array(int arr[], int size) {
+    int total=0;
+    for (int i=0; i < size; i++) {
+        total+=arr[i];
     }
     return total;
 }
+int main()
+{
+int arr[5]={10,20,30,40,50};
+int size=5;
 
-int main() {
-    int arr[5] = {1, 32, 43, 54, 65};  
-    int size = 5;  
+ 
+    int total = array(arr, size);
+    printf("The addition of the function=%d\n",total);
 
     
-    int total = Array(arr, size);
 
-    
-    printf("The sum of the array elements is: %d\n", total);
 
     return 0;
 }
