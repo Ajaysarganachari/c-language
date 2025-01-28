1efa1c8d1aa026182d32a2e8b2e1d07e383d2749
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-01-28 11:52:23 +0530

    Update file.c

--- file.c
+++ file.c
@@ -10,28 +10,38 @@ int main()
         printf("file does not exist\n");
     }
     
-char temp;
-int cnt=0;
-int wordcount=0;
-int linecount=0;
-while((temp=fgetc(fptr))!=EOF){
-    printf("temp=%c\n",temp);
-    printf("temp=%d",temp);
-    cnt++;
-}
-  printf("no.of character:%d\n",cnt);
-    int tword=(int)temp;
-    printf("temp=%c\n",temp);
-    cnt++;
+// char temp;
+// int cnt=0;
+// int wordcount=0;
+// int linecount=0;
+// while((temp=fgetc(fptr))!=EOF){
+//     printf("temp=%c\n",temp);
+//     printf("temp=%d",temp);
+//     cnt++;
+// }
+//   printf("no.of character:%d\n",cnt);
+//     int tword=(int)temp;
+//     printf("temp=%c\n",temp);
+//     cnt++;
 
-    printf("no.of character=%d",cnt);
-    if(tword==32){
-        wordcount++;
-        if(tword==10){
-            linecount++;
-        }
-    }
-    printf("word count:%d\n",wordcount);
+//     printf("no.of character=%d",cnt);
+//     if(tword==32){
+//         wordcount++;
+//         if(tword==10){
+//             linecount++;
+//         }
+//     }
+//     printf("word count:%d\n",wordcount);
     
-    return 0;
+    // char str[100];
+    // while((fgets(str,100,fptr))!=NULL){
+    //     printf("line=%c\n",str);
+    // }
+      int empid;
+      char name[100],desg[100];
+      fscanf(fptr,"%d %s %s",&empid,&name,&desg);
+      printf("empid=%d\n",empid);
+      printf("name=%s\n",name);
+      printf("desg=%s\n",desg);
+          return 0;
 }
